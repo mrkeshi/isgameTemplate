440670246187d40e7f8dde8712d0d31006153187
mrkeshi <mr.alireza.keshavarz@gmail.com>
2023-09-12 17:28:41 +0330

    init production

--- dist/slider.js
+++ dist/slider.js
@@ -1,4 +1,42 @@
 import Swiper from 'swiper';
 // import Swiper styles
 
-const swiper = new Swiper()
+const swiper = new Swiper('#swiper', {
+    // Optional parameters
+
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '#prevslide',
+      prevEl: '#nextslide',
+    },
+
+    // And if we need scrollbar
+
+  });
+
+  const swiper2 = new Swiper('#swiper2', {
+    // Optional parameters
+
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '#swiper-pagination2',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '#prevslide2',
+      prevEl: '#nextslide2',
+    },
+
+    // And if we need scrollbar
+
+  });
